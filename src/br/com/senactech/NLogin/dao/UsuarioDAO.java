e3945a27061f0dac0c70a235ffb4223fc9e4c9f4
Jair Borges Ferraz <jbferraz@senacinformatica.br>
2022-07-19 21:46:14 -0300

    CRUD Usuario | jFrame cadUsuario Cadastrando

--- src/br/com/senactech/NLogin/dao/UsuarioDAO.java
+++ src/br/com/senactech/NLogin/dao/UsuarioDAO.java
@@ -6,10 +6,18 @@ package br.com.senactech.NLogin.dao;
 
 import br.com.senactech.NLogin.conexao.Conexao;
 import br.com.senactech.NLogin.model.Usuario;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Statement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import static nlogin.NLogin.geraSenha;
 
 /**
  *
@@ -17,12 +25,9 @@ import java.sql.ResultSet;
  */
 public class UsuarioDAO {
 
-    public void cadUsuario(Usuario uVO) throws SQLException {
-        //Abre conexão com BD
-        Connection con = Conexao.getConexao();
-        //Cria área de execução de script SQL
-        Statement stat = con.createStatement();
-        try {
+    public void cadUsuario(Usuario uVO){        
+        try(Connection con = Conexao.getConexao();
+                Statement stat = con.createStatement();) {
             String sql;
             sql = "insert into usuario values(null,"
                     + "'" + uVO.getNomeUsuario() + "',"
@@ -30,11 +35,10 @@ public class UsuarioDAO {
                     + "'" + uVO.getSenha() + "')";
             stat.execute(sql);
         } catch (SQLException e) {
-            throw new SQLException("Erro ao inserir.\n" + e.getMessage());
-        } finally {
-            con.close();
-            stat.close();
-        }
+            JOptionPane.showMessageDialog(null,
+                    "Erro ao inserir.\n" + e.getMessage(),
+                    "Erro",JOptionPane.ERROR_MESSAGE);
+        } 
     }
 
     public Usuario getByUsuario(String user) throws SQLException {
@@ -55,10 +59,75 @@ public class UsuarioDAO {
             }
         } catch (SQLException e) {
             throw new SQLException("Usuario não encontrado.\n" + e.getMessage());
-        }finally{
+        } finally {
             con.close();
             stat.close();
         }
         return u;
+    }
+
+    public ArrayList<Usuario> listaUsuarios() throws SQLException {
+        //Abre conexão com BD
+        Connection con = Conexao.getConexao();
+        //Cria área de execução de script SQL
+        Statement stat = con.createStatement();
+        try {
+            String sql;
+            sql = "select idUsuario, nomeUsuario, usuario from usuario";
+            ResultSet rs = stat.executeQuery(sql);
+            ArrayList<Usuario> usuarios = new ArrayList<>();
+            while (rs.next()) {
+                Usuario u = new Usuario();
+                u.setIdUsuario(rs.getInt("idUsuario"));
+                u.setNomeUsuario(rs.getString("nomeUsuario"));
+                u.setUsuario(rs.getString("usuario"));
+                usuarios.add(u);
+            }
+            return usuarios;
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao listar usuários!\n"
+                    + e.getMessage());
+        } finally {
+            con.close();
+            stat.close();
+        }
+    }
+
+    public void atualizarUsuario(Usuario uVO) throws SQLException,
+            NoSuchAlgorithmException, UnsupportedEncodingException {
+        //Abre conexão com BD
+        Connection con = Conexao.getConexao();
+        //Cria área de execução de script SQL
+        //Statement stat = con.createStatement();
+        String sql;
+        sql = "update usuario set nomeUsuario = ?, senha = ? where idUsuario = ?";
+        PreparedStatement pStat = con.prepareStatement(sql);
+        try {
+            pStat.setString(1, uVO.getNomeUsuario());
+            pStat.setString(2, geraSenha(uVO.getSenha()));
+            pStat.setInt(3, uVO.getIdUsuario());
+            pStat.executeUpdate();
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao atualizar.\n" + e.getMessage());
+        } finally {
+            pStat.close();
+            con.close();
+        }
+    }
+
+    public void deletarUsuario(int id) throws SQLException {
+        //Abre conexão com BD
+        Connection con = Conexao.getConexao();
+        //Cria área de execução de script SQL
+        String sql;
+        sql = "delete from usuario where idUsuario = ?";
+        try (PreparedStatement pStat = con.prepareStatement(sql)) {
+            pStat.setInt(1, id);
+            pStat.execute();
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao deletar.\n" + e.getMessage());
+        } finally {
+            con.close();
+        }
     }
 }
--- src/br/com/senactech/NLogin/services/UsuarioServicos.java
+++ src/br/com/senactech/NLogin/services/UsuarioServicos.java
@@ -7,7 +7,10 @@ package br.com.senactech.NLogin.services;
 import br.com.senactech.NLogin.dao.DAOFactory;
 import br.com.senactech.NLogin.dao.UsuarioDAO;
 import br.com.senactech.NLogin.model.Usuario;
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 /**
  *
@@ -15,13 +18,26 @@ import java.sql.SQLException;
  */
 public class UsuarioServicos {
 
-    public void cadUsuario(Usuario uVO) throws SQLException {
-        UsuarioDAO uDAO = DAOFactory.getUsuarioDAO();
+    private final UsuarioDAO uDAO = DAOFactory.getUsuarioDAO();
+
+    public void cadUsuario(Usuario uVO) {
         uDAO.cadUsuario(uVO);
     }
-    
-    public Usuario getByUsuario(String user) throws SQLException{
-        UsuarioDAO uDAO = DAOFactory.getUsuarioDAO();
+
+    public Usuario getByUsuario(String user) throws SQLException {
         return uDAO.getByUsuario(user);
+    }
+
+    public ArrayList<Usuario> getUsuarios() throws SQLException {
+        return uDAO.listaUsuarios();
+    }
+
+    public void atualizarUsuario(Usuario uVO) throws SQLException,
+            NoSuchAlgorithmException, UnsupportedEncodingException {
+        uDAO.atualizarUsuario(uVO);
+    }
+
+    public void deletarUsuario(int id) throws SQLException {
+        uDAO.deletarUsuario(id);
     }
 }
